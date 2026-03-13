09873957500d772f5a26f55b9abe39f2f552052c
Marcus Birkenkrahe <377079+birkenkrahe@users.noreply.github.com>
2026-03-12 21:41:51 -0500

    Update from thelio

---
+++ src/range.c
@@ -0,0 +1,18 @@
+// range.c: compute condition with range
+// input: none. output: Boolean
+// author: Marcus Birkenkrahe GPLv3
+// date: 2/27/25
+#include <stdio.h>
+
+int main(void)
+{
+  int i, m, n;
+  scanf("%d %d %d", &i, &m, &n);
+
+  if (...)
+    printf("%d is in the interval [%d,%d).\n", i, m, n);
+  else
+    printf("%d is NOT in the interval [%d,%d).\n", i, m, n);
+
+  return 0;
+}
